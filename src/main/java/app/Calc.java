df5e6dbce420873ae8308791b9d6316e99ed83da
modhi-java <Modhi2585>
2018-04-08 22:48:12 +0300

    العوامل الحسابية

--- src/main/java/app/Calc.java
+++ src/main/java/app/Calc.java
@@ -5,10 +5,29 @@ public class Calc {
 //المعاملات الحسابية إضافة واحد  ++
 public static void main(String[] args){
 
-    int x = 1;
-    x++;
-    System.out.print(++x);
+//    int x = 1;
+//    x++;
+//    System.out.print(++x);
+//
 
+//درس عوامل المقارنات
+    int x = 10;
+    int y = 20;
+    System.out.println(x==y);
+    // إذا كان 2 يساوي معناه انه يسألأ هل الاكس يساوي واي
+
+    System.out.println(x!=y);
+    //هل الأكس لايساوي الواي
+
+    System.out.println(x>y);
+//هل الاكس اكبر من واي
+
+
+    System.out.println(x<y);
+
+    System.out.println(x>=y);
+//أكبر أو يساوي
+    System.out.println(x<=y);
 
 }
 }
